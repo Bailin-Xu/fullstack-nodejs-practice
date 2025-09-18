a160f49914152a7933f65295668b18c9b6782376
Bailin-Xu <xbldfs@gmail.com>
2025-09-18 17:40:25 -0400

    delete root route in backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,9 @@ const cors = require('cors')
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
+
+
 
 let notes = [
     {
@@ -28,10 +31,6 @@ let notes = [
         important: true
     }
 ]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
